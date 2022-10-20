2f9395bb8b2cbe01ef1b64f6035bc9f57601cf02
Alan Bedian <alan.bedian@gmail.com>
2022-10-20 01:25:59 -0700

    Added prompt tweening to megapipe & disabled nsfw log in interpolation

--- examples/community/interpolate_stable_diffusion.py
+++ examples/community/interpolate_stable_diffusion.py
@@ -100,7 +100,8 @@ class StableDiffusionWalkPipeline(DiffusionPipeline):
             new_config["steps_offset"] = 1
             scheduler._internal_dict = FrozenDict(new_config)
 
-        if safety_checker is None:
+        #if safety_checker is None:
+        if False:
             logger.warn(
                 f"You have disabled the safety checker for {self.__class__} by passing `safety_checker=None`. Ensure"
                 " that you abide to the conditions of the Stable Diffusion license and do not expose unfiltered"
