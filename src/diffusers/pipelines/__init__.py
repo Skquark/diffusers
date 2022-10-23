5c17b6fe425bd80b7689bcb3bcd2ab73df0648f0
Alan Bedian <alan.bedian@gmail.com>
2022-10-23 00:03:56 -0700

    Removed my UnifiedPipeline import which caused an error

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -18,7 +18,7 @@ if is_torch_available() and is_transformers_available():
         StableDiffusionInpaintPipeline,
         StableDiffusionInpaintPipelineLegacy,
         StableDiffusionPipeline,
-        UnifiedPipeline,
+        #UnifiedPipeline,
     )
 
 if is_transformers_available() and is_onnx_available():
